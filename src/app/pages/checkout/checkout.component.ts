9d0471cbcd084a3c27a7c11aa5c7786c6b58039b
virshu <felix@rabinovich.org>
2025-11-28 13:52:49 -0800

    Checkout (without CSS)

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss',
 })
 export class CheckoutComponent {
+  cart = inject(CartService);
+  router = inject(Router);
+
+  readonly items = this.cart.items;
+  readonly totalPrice = this.cart.totalPrice;
+  readonly isEmpty = this.cart.isEmpty;
+  readonly confirmed = signal(false);
+
+  confirmOrder() {
+    this.confirmed.set(true);
+    this.cart.clearCart();
+    // Optionally navigate to a confirmation page or home
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 3000);
+  }
 
 }
