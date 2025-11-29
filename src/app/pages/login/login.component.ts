255c23af7554273e9627e66d27579d35aef24c5f
virshu <felix@rabinovich.org>
2025-11-29 10:53:13 -0800

    Signal Forms (basic)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,19 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { LoginRequest } from './login-request';
+import { form, Field } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [Field],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
   private readonly auth = inject(AuthService);
   private readonly router = inject(Router);
+  readonly loginModel = signal<LoginRequest>({
+      username: '',
+      password: ''
+    });
+  readonly loginForm = form(this.loginModel);
 
-  login() {
-    this.auth.login('exampleUser');
+  login(event: Event): void {
+    event.preventDefault();
+    this.auth.login(this.loginModel());
     this.router.navigateByUrl('/');
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,28 @@
 import { computed, Injectable, signal } from '@angular/core';
+import { LoginRequest } from '../pages/login/login-request';
+import { HttpClient } from '@angular/common/http';
+import { LoginResponse } from '../pages/login/login-response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  #user = signal<string | null>(null);
+  #user = signal<LoginResponse | null>(null);
 
   readonly isLoggedIn = computed(() => this.#user() !== null);
   readonly username = computed(() => this.#user());
 
-  login(username: string): void {
-    this.#user.set(username);
-  }
+  private apiUrl = 'https://fakestoreapi.com/auth/login';
+  constructor(private http: HttpClient) {}
+
+  login(username: LoginRequest): void {
+    const headers = { 'Content-Type': 'application/json' };
+    this.http.post<{ token: string }>(this.apiUrl, username, { headers }).subscribe({
+      next: (response) => {
+        console.log('Login successful, token:', response.token);
+        this.#user.set({ username: username.username, token: response.token });
+  }})
+}
 
   logout(): void {
     this.#user.set(null);
